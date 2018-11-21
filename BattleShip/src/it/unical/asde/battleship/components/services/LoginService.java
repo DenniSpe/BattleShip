698d3085e0541f2c9216fe72aada1938a28465e8
maborroto <maborroto1987@gmail.com>
2018-11-21 06:22:14 -0500

    Changing DAO Declaration

--- BattleShip/src/it/unical/asde/battleship/components/services/LoginService.java
+++ BattleShip/src/it/unical/asde/battleship/components/services/LoginService.java
@@ -3,14 +3,14 @@ package it.unical.asde.battleship.components.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import it.unical.asde.battleship.components.persistence.UsersDAO;
+import it.unical.asde.battleship.components.persistence.impl.UsersDAOImpl;
 import it.unical.asde.battleship.model.User;
 
 @Service
 public class LoginService {
 	
 	@Autowired
-	private UsersDAO usersDAO;
+	private UsersDAOImpl usersDAO;
 	
 	public boolean checkCredentials(String uname, String pwd) {
 		return usersDAO.checkCredentials(new User(uname, pwd));
